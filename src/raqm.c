b8d0ba2c8cea52a7366c250c105dc1ff0133d8fd
Khaled Hosny <khaledhosny@eglug.org>
2016-09-18 17:56:53 +0200

    Use switch statement

--- src/raqm.c
+++ src/raqm.c
@@ -1368,9 +1368,9 @@ _raqm_line_break (raqm_t *rq)
   }
 
   /* handeling alighnment */
-  if (rq->alignment != RAQM_ALIGNMENT_LEFT)
+  switch (rq->alignment)
   {
-    if (rq->alignment == RAQM_ALIGNMENT_RIGHT)
+    case RAQM_ALIGNMENT_RIGHT:
     {
       size_t j = 0;
       int line = -1;
@@ -1402,9 +1402,9 @@ _raqm_line_break (raqm_t *rq)
         }
         i = j + 1;
       }
+      break;
     }
-
-    if (rq->alignment == RAQM_ALIGNMENT_CENTER)
+    case RAQM_ALIGNMENT_CENTER:
     {
       size_t j = 0;
       current_line = -1;
@@ -1419,9 +1419,9 @@ _raqm_line_break (raqm_t *rq)
         }
         i = j + 1;
       }
+      break;
     }
-
-    if (rq->alignment == RAQM_ALIGNMENT_JUSTIFY)
+    case RAQM_ALIGNMENT_JUSTIFY:
     {
       int space_count = 0;
       size_t j = 0;
@@ -1455,7 +1455,11 @@ _raqm_line_break (raqm_t *rq)
         }
         i = j + 1;
       }
+      break;
     }
+    case RAQM_ALIGNMENT_LEFT:
+    default:
+      break;
   }
 
   free (line_breaks);
